c92456d913f64a1d4114983156b0fc232ab6b25c
RKrumova <77579829+RKrumova@users.noreply.github.com>
2021-07-04 21:07:50 +0300

    Create CinemaRoomsManagment.java

---
+++ CinemaRoomsManagment.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class CinemaRoomsManagment {
+
+    public static void main(String[] args) {
+        
+        System.out.print("Cinema:\n ");
+        for(int i =0; i<8; i++){
+            System.out.print(" " + (i+1));
+        }
+        System.out.println();
+        for(int a = 1; a < 8; a++) {
+            System.out.print(a);
+            for(int i = 0; i<8; i++){
+                System.out.print(" S");
+            }
+        System.out.println();
+        }
+    }
+}
